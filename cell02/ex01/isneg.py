ecfd9706a2bf5a53cbca157e863d02b78bd0842a
Dearsaran24 <saranintasote@gmail.com>
2025-06-18 15:43:12 +0700

    Create isneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+zome = int(input())
+if zome > 0 :
+  print("This number is positive.")
+elif zome < 0 :
+  print("This number is negative.")
+elif zome == 0 :
+  print("This number is both positive and negative.")
