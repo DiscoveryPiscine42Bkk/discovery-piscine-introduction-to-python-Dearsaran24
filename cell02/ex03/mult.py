5ff1c85022f011705931bed12cae4a61834ea24e
Dearsaran24 <saranintasote@gmail.com>
2025-06-18 17:10:58 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,1 +1,10 @@
-
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+num3 = num1*num2
+print(f"{num1} x {num2} = {num3}")
+if num3 > 0 :
+  print("The result is positive.")
+elif num3 < 0 :
+  print("The result is negative.")
+elif num3 == 0 :
+  print("The result is positive and negative.")
